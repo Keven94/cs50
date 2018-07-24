7d7da506c6f78d346f8ff47d27f40b8033fa6c37
Keven94 <40154212+Keven94@users.noreply.github.com>
2018-07-24 00:38:36 -0500

    Update mario.c

--- pset1/mario/Less comfortable/mario.c
+++ pset1/mario/Less comfortable/mario.c
@@ -1,1 +1,27 @@
-about to work on the code
+#include <stdio.h>
+#include <cs50.h>
+
+int main()
+{
+    int h;
+
+    //prompt the user for the half-pyramid’s height, a non-negative integer no greater than 23
+    do
+    {
+        h = get_int("Height: ");
+    }
+    while (h < 0 || h > 23);
+
+    for (int i = 0; i < h; i++)
+    {
+        for (int j = i; j < h - 1; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 0; k < i + 1; k++)
+        {
+            printf("#");
+        }
+        printf("#\n");
+    }
+}
